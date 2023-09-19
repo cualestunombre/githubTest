80e0b3fa1be2796244af72f0cfb0b35571123b07
fearlessdeveloper <1dilumn0@gmail.com>
2023-09-19 13:20:19 +0900

    서버 deploy용

---
+++ authentication/src/test/java/webdoc/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package webdoc.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
