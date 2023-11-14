720ac66b1db14ee9f59f64d5f88a80a26463a45c
fearlessdeveloper <1dilumn0@gmail.com>
2023-11-14 22:23:17 +0900

    authentication 삭제

--- authentication/src/main/java/webdoc/authentication/domain/exceptions/TimeOutException.java
+++
@@ -1,7 +0,0 @@
-package webdoc.authentication.domain.exceptions;
-
-public class TimeOutException extends RuntimeException{
-    public TimeOutException(String message) {
-        super(message);
-    }
-}
